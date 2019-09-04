25624caebd2df3947fcaf6ce64956a7acb3e9ea8
Bikal Lem <gbikal@gmail.com>
2019-09-04 18:51:52 +0100

    Use Lwt_ssl.shutdown_ssl() instead of shutdown for graceful exiting

--- examples/stalkd.ml
+++ examples/stalkd.ml
@@ -6,16 +6,16 @@ let sp = Printf.sprintf
 
 let establish_server connection_handler ~port ~max_backlog_conn =
   let addr_string = function
-    | Unix.ADDR_INET (n, p) ->
-        sp "%s: %s" (Unix.string_of_inet_addr n) (string_of_int p)
-    | Unix.ADDR_UNIX _ ->
-        sp "%s" Unix.(string_of_inet_addr inet_addr_any)
+  | Unix.ADDR_INET (n, p) ->
+    sp "%s: %s" (Unix.string_of_inet_addr n) (string_of_int p)
+  | Unix.ADDR_UNIX _ ->
+    sp "%s" Unix.(string_of_inet_addr inet_addr_any)
   in
   let handle_client_connection (client_ssl, client_addr) =
     Lwt.async (fun () ->
-        log (sp "opening connection for [%s]" @@ addr_string client_addr)
-        <&> connection_handler client_addr client_ssl
-        >|= fun () -> Lwt_ssl.shutdown client_ssl Unix.SHUTDOWN_ALL)
+      log (sp "opening connection for [%s]" @@ addr_string client_addr)
+      <&> connection_handler client_addr client_ssl
+      >|= fun () -> Lwt_ssl.ssl_shutdown client_ssl)
   in
   let rec accept_clients server_sock ssl_ctx =
     Lwt.catch
@@ -50,30 +50,30 @@ let connected_clients = ref []
 let connection_handler client_addr client_ssl =
   connected_clients := (client_addr, client_ssl) :: !connected_clients ;
   let rec talk = function
-    | "exit" ->
-        log "A client has quit"
-        >|= fun () ->
-        connected_clients :=
-          List.filter
-            (fun (_, client_ssl') -> client_ssl' != client_ssl)
-            !connected_clients
-    | _ ->
-        Lwt_ssl.read client_ssl buf 0 bufsize
-        >>= fun l ->
-        let m = Bytes.sub buf 0 l in
-        let msg = Bytes.sub m 0 (Bytes.length m - 1) in
-        let msg = Bytes.to_string msg in
-        log (sp "received '%s'" msg)
-        >>= fun () ->
-        List.iter
-          (fun (_, lwt_ssl') ->
-            match Lwt_ssl.ssl_socket lwt_ssl' with
-            | None ->
-                ()
-            | Some s ->
-                Ssl.output_string s (Bytes.to_string m))
-          !connected_clients ;
-        talk msg
+  | "exit" ->
+    log "A client has quit"
+    >|= fun () ->
+    connected_clients :=
+      List.filter
+        (fun (_, client_ssl') -> client_ssl' != client_ssl)
+        !connected_clients
+  | _ ->
+    Lwt_ssl.read client_ssl buf 0 bufsize
+    >>= fun l ->
+    let m = Bytes.sub buf 0 l in
+    let msg = Bytes.sub m 0 (Bytes.length m - 1) in
+    let msg = Bytes.to_string msg in
+    log (sp "received '%s'" msg)
+    >>= fun () ->
+    List.iter
+      (fun (_, lwt_ssl') ->
+        match Lwt_ssl.ssl_socket lwt_ssl' with
+        | None ->
+          ()
+        | Some s ->
+          Ssl.output_string s (Bytes.to_string m))
+      !connected_clients ;
+    talk msg
   in
   log "accepted a new connection" >>= fun () -> talk ""
 
